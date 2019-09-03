ef42ed91ab8553fe8cd3cb62dca3912bc21c394d
vladymyr.zakharov <vladymyr.zakharov@ab-soft.net>
2019-09-03 13:21:08 +0300

    work with fakeNewsActivity

--- app/src/main/java/com/example/myapplication/ui/FakeNewsActivity.java
+++ app/src/main/java/com/example/myapplication/ui/FakeNewsActivity.java
@@ -35,6 +35,7 @@ import java.util.Objects;
 public class FakeNewsActivity extends AppCompatActivity {
 
     private TextInputEditText title;
+    private TextInputLayout titleLayout;
 
     private TextInputEditText description;
 
@@ -49,7 +50,6 @@ public class FakeNewsActivity extends AppCompatActivity {
     private ImageView uploadedImage;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Slide slideIn = new Slide();
@@ -61,77 +61,93 @@ public class FakeNewsActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-    title = findViewById(R.id.user_input_title);
-    description = findViewById(R.id.user_input_description);
-    content = findViewById(R.id.user_input_content);
+        title = findViewById(R.id.user_input_title);
+        titleLayout = findViewById(R.id.title_text_input_layout);
+        description = findViewById(R.id.user_input_description);
+        content = findViewById(R.id.user_input_content);
         Button submitButton = findViewById(R.id.submit_button);
-    fakeNewsContainer = findViewById(R.id.fake_news_container);
+        fakeNewsContainer = findViewById(R.id.fake_news_container);
 
-    ImageButton uploadImageButton = findViewById(R.id.upload_image_button);
-    uploadedImage = findViewById(R.id.user_image_upload);
+        ImageButton uploadImageButton = findViewById(R.id.upload_image_button);
+        uploadedImage = findViewById(R.id.user_image_upload);
         uploadImageButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(galleryIntent, LOAD_IMAGE);
-        }
-    });
-    ImageButton useCameraButton = findViewById(R.id.user_camera_button);
-        useCameraButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-                startActivityForResult(takePictureIntent, USE_CAMERA);
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, LOAD_IMAGE);
             }
-        }
-    });
+        });
+        ImageButton useCameraButton = findViewById(R.id.user_camera_button);
+        useCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, USE_CAMERA);
+                }
+            }
+        });
 
         submitButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(TextUtils.isEmpty(Objects.requireNonNull(title).getText().toString()) || TextUtils.isEmpty(description.getText().toString())){
-                Snackbar.make(findViewById(R.id.fake_news_activity), "Please enter a title and a description", Snackbar.LENGTH_SHORT).show();
-            }
-            else {
-                Intent madeFakeNewsIntent = new Intent(FakeNewsActivity.this, MadeFakeNews.class);
-                madeFakeNewsIntent.putExtra("fakeNewsTitle", title.getText().toString());
-                madeFakeNewsIntent.putExtra("fakeNewsDescription", description.getText().toString());
-                madeFakeNewsIntent.putExtra("fakeNewsContent", content.getText().toString());
-                madeFakeNewsIntent.putExtra("fakeNewsImage", ((BitmapDrawable) uploadedImage.getDrawable()).getBitmap());
-                Pair<View, String> titlePair = Pair.create((View) title, getString(R.string.fake_news_transition_title));
-                Pair<View, String> descriptionPair = Pair.create((View) description, getString(R.string.fake_news_transition_description));
-                Pair<View, String> contentPair = Pair.create((View) content, getString(R.string.fake_news_transition_content));
-                Pair<View, String> imagePair = Pair.create((View) uploadedImage, getString(R.string.fake_news_transition_image));
-                ActivityOptions options = ActivityOptions
-                        .makeSceneTransitionAnimation(FakeNewsActivity.this,
-                                titlePair,
-                                descriptionPair,
-                                contentPair,
-                                imagePair);
-                startActivity(madeFakeNewsIntent, options.toBundle());
+            @Override
+            public void onClick(View view) {
+                titleLayout.setError(null);
+                View view1 = null;
+                boolean isError = false;
 
-            }
-        }
-    });
-}
+                if (TextUtils.isEmpty(Objects.requireNonNull(title).getText().toString()) || TextUtils.isEmpty(description.getText().toString())) {
+                    Snackbar.make(findViewById(R.id.fake_news_activity), "Please enter a title and a description", Snackbar.LENGTH_SHORT).show();
+                    if (TextUtils.isEmpty(Objects.requireNonNull(title).getText().toString())){
+                        titleLayout.setError("Error");
+                        view1 = title;
+                        isError = true;
+                    }
 
+                    if (isError){
+
+                        view1.setFocusable(true);
+                    }
+
+
+                } else {
+                    Intent madeFakeNewsIntent = new Intent(FakeNewsActivity.this, MadeFakeNews.class);
+                    madeFakeNewsIntent.putExtra("fakeNewsTitle", title.getText().toString());
+                    madeFakeNewsIntent.putExtra("fakeNewsDescription", description.getText().toString());
+                    madeFakeNewsIntent.putExtra("fakeNewsContent", content.getText().toString());
+//                    madeFakeNewsIntent.putExtra("fakeNewsImage", ((BitmapDrawable) uploadedImage.getDrawable()).getBitmap());
+                    Pair<View, String> titlePair = Pair.create((View) title, getString(R.string.fake_news_transition_title));
+                    Pair<View, String> descriptionPair = Pair.create((View) description, getString(R.string.fake_news_transition_description));
+                    Pair<View, String> contentPair = Pair.create((View) content, getString(R.string.fake_news_transition_content));
+                    Pair<View, String> imagePair = Pair.create((View) uploadedImage, getString(R.string.fake_news_transition_image));
+                    ActivityOptions options = ActivityOptions
+                            .makeSceneTransitionAnimation(FakeNewsActivity.this,
+                                    titlePair,
+                                    descriptionPair,
+                                    contentPair,
+                                    imagePair);
+                    startActivity(madeFakeNewsIntent, options.toBundle());
+
+                }
+            }
+        });
+    }
 
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+        if (requestCode == LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
             Uri selectedImage = data.getData();
+//            selectedImage.getPath()
             uploadedImage.setImageURI(selectedImage);
             BitmapDrawable drawable = (BitmapDrawable) uploadedImage.getDrawable();
             Bitmap bitmap = drawable.getBitmap();
-            uploadedImage.setImageBitmap(ImageUtils.getRoundedCornerBitmap(bitmap, 80, 80,80,80,80,80,80,80));
+            uploadedImage.setImageBitmap(ImageUtils.getRoundedCornerBitmap(bitmap, 80, 80, 80, 80, 80, 80, 80, 80));
         }
-        if(requestCode == USE_CAMERA && resultCode == RESULT_OK && data != null){
+        if (requestCode == USE_CAMERA && resultCode == RESULT_OK && data != null) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) Objects.requireNonNull(extras).get("data");
-            Bitmap roundedBitmap = ImageUtils.getRoundedCornerBitmap(imageBitmap, 80, 80,80,80,80,80,80,80);
+            Bitmap roundedBitmap = ImageUtils.getRoundedCornerBitmap(imageBitmap, 80, 80, 80, 80, 80, 80, 80, 80);
             uploadedImage.setImageBitmap(roundedBitmap);
         }
     }
