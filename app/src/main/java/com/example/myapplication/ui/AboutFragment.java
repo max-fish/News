65365d6227924537f4e68b390b33163aaa00dacf
vladymyr.zakharov <vladymyr.zakharov@ab-soft.net>
2019-09-10 13:56:26 +0300

    fixed animation between news activities and add scroll to fakeActivity

--- app/src/main/java/com/example/myapplication/ui/AboutFragment.java
+++ app/src/main/java/com/example/myapplication/ui/AboutFragment.java
@@ -11,6 +11,7 @@ import android.widget.ImageButton;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.example.myapplication.ui.news.FakeNewsActivity;
 
 public class AboutFragment extends Fragment {
 
